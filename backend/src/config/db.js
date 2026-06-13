a178bc615ddb5da9c415eb6aacb77b93b1c1496b
AlisherAbsaitov <alisher24032002@gmail.com>
2026-06-13 11:30:13 +0500

    add mongodb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,9 @@ dotenv.config()
  */
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://abdulloh9696:FZNGoPo4V3oZLcZw@cluster0.g7r9ryh.mongodb.net/?appName=Cluster0",
+    );
     console.log(`✅ MongoDB ulandi: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB ulanish xatosi: ${error.message}`);
